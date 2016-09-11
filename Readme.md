1a02340f3bbb988eb231b59269b9f10f03685d9e
Le An <le.an@polymtl.ca>
2016-09-11 13:02:09 -0400

    - Update Readme - Add modelling scripts

--- Readme.md
+++ Readme.md
@@ -16,6 +16,7 @@
 	- **commit_bug_mapping.py**: map bug-fixing commits to their corresponding bugs.
 	- **fault_inducing.py**: identify bug-inducing commits based on the SZZ algorithm.
 - **statistics** folder contains data extracted from software repositories and statistical analysis scripts. For each subject system, we extract its basic metrics; and from the basic metrics we further extract its metrics for modelling.
+  - **modelling** folder contains R scripts to build the GLM model for each subject system using a specific clone detection tool.
 - **raw_data** folder contains the commit logs and commit sequence for each subject system extracted from its Git repository.
 - **output_data.zip** is a compressed folder that contains the results of clone classes, clone genealogies, and bug-inducing commits.
 
@@ -23,14 +24,14 @@
 1. Clone a project's Git repository. For systems originally managed by SVN, please follow this tutorial to clone the repository as Git:
    https://www.atlassian.com/git/tutorials/migrating-convert/.
 2. Use the following command to extract the project's commit logs:
-	```git log --pretty=format:"%H,%ae,%ai,%s"```
+	```git log --pretty=format:"%H,%ae,%ai,%s"```.
 3. Uncompress the folder ```output_data.zip```.
 4. Run **detect_clones.py** and **extract_clone_results.py** to detect clone classes for a subject system using a clone detection tool.
 6. Run **build_genealogies.py** to extract clone pairs from the JSON file, then build clone genealogies for each clone pair.
 7. Run **commit_bug_mapping.py** and **fault_inducing.py** to identify bug-inducing commits.
 8. Run **analyse_genealogies.py** to perform Fisher's exact test for RQ1 and RQ2.
 9. Run **independant_variables.py** to extract explanatory variables for RQ3.
-
+10. Build GLM models with the R script in the **statistics/modelling** folder.
 
 ###Data source
 - Source code repositories:
---
+++ statistics/modelling/jedit_nicad.R
@@ -0,0 +1,76 @@
+library(car)
+library(caret)
+library(modEvA)
+library(plyr)
+
+project = 'jedit'
+tool = 'nicad'
+
+# Load data as a data frame
+df = as.data.frame(read.csv(file=sprintf('../%s/%s_metrics.csv', tool, project)), header=TRUE)
+cols = c(2:3, 5:11, 13:19)
+df.log = log(df[,cols] + 1)
+df.log$CFltFix = df$CFltFix
+df.log$CCurSt = df$CCurSt
+df.log$EEvPattern = df$EEvPattern
+df.log$buggy = df$buggy
+
+head(df.log)
+
+##### build model M(s) #####
+xcol = c('cloc','CFltFix','CPathDepth','CCurSt','experience')
+formula = as.formula(paste('buggy ~ ', paste(xcol, collapse= '+')))
+fit1 = glm(formula, data = df.log, family = binomial())
+# coefficients and p-value of M(s)
+vif(fit1)
+fit1
+summary(fit1)
+# deviance explained of M(s)
+Dsquared(fit1)
+# variable importance in M(s)
+varImp(fit1, scale = FALSE)
+
+##### build model M(s+e) #####
+xcol = c(xcol, c('EFltDens','TChurn','TPC','NumOfBursts','SLBurst','CFltRate'))
+formula = as.formula(paste('buggy ~ ', paste(xcol, collapse= '+')))
+fit2 = glm(formula, data = df.log, family = binomial())
+# VIF analysis
+vif(fit2)
+formula = update(formula, ~. -NumOfBursts)
+fit2 = glm(formula, data = df.log, family = binomial())
+vif(fit2)
+# show coefficients and p-value of M(s+e)
+fit2
+summary(fit2)
+# compare M(s+e) again M(s)
+anova(fit1, fit2, test='Chisq')
+# deviance explained of M(s+e)
+Dsquared(fit2)
+
+
+##### build model M(s+e+g) #####
+xcol = c(xcol, c('EEvPattern','EConChg','EIncChg','EConStChg','EIncStChg','EFltsConStChg','EFltIncStChg','EChgTimeInt'))
+formula = as.formula(paste('buggy ~ ', paste(xcol, collapse= '+')))
+fit3 = glm(formula, data = df.log, family = binomial())
+# VIF analysis
+alias(fit3)
+formula = update(formula, ~. -EEvPattern -EIncChg -EConStChg -EFltsConStChg -EFltIncStChg)
+fit3 = glm(formula, data = df.log, family = binomial())
+alias(fit3)
+vif(fit3)
+formula = update(formula, ~. -TPC -EIncStChg -EConChg -EFltDens)
+fit3 = glm(formula, data = df.log, family = binomial())
+vif(fit3)
+# show coefficients and p-value of M(s+e+g)
+fit3
+summary(fit3)
+# compare M(s+e+g) again M(s+e)
+anova(fit2, fit3, test='Chisq')
+# deviance explained of M(s+e+g)
+Dsquared(fit3)
+# variable importance in M(s+e+g)
+imp = varImp(fit3)
+imp$Variable = rownames(imp)
+imp = imp[c('Variable', 'Overall')]
+arrange(imp,desc(Overall))
+
